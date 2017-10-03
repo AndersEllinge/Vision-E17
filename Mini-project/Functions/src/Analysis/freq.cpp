1a6772e4fe93e4612f56c3578cf1cefcbf8cfbd7
Lordgreger <blacknedpsi@gmail.com>
2017-10-03 14:30:04 +0200

    fixed freq

--- Mini-project/Functions/src/Analysis/freq.cpp
+++ Mini-project/Functions/src/Analysis/freq.cpp
@@ -3,46 +3,62 @@
 
 void dftshift(cv::Mat& mag);
 
-int main( int argc, char** argv )
+int main(int argc, char* argv[])
 {
-    cv::Mat src;
+    cv::CommandLineParser parser(argc, argv,
+        "{@image | ./lena.bmp | image path}"
+    );
 
-    // Load image
-    src = cv::imread( argv[1], 0);
+    // Load image as grayscale
+    std::string filename = parser.get<std::string>("@image");
+    cv::Mat img = cv::imread(filename, cv::IMREAD_GRAYSCALE);
 
-    if( !src.data )
-        { return -1; }
+    if (img.empty()) {
+        std::cout << "Input image not found at '" << filename << "'\n";
+        return -1;
+    }
 
-    // Do dft?
-    cv::Mat I = src.clone();
-    cv::Mat planes[] = {cv::Mat_<float>(I), cv::Mat_<float>::zeros(I.size())};
-    cv::Mat dft;
-    cv::merge(planes, 2, dft);
-
-    std::cout << "Channels: " << dft.channels() << std::endl;
-
-    cv::dft(dft, dft);
-
-    // Split into amplitude and phase channel
-    cv::Mat dftRes[2];
-    cv::split(dft, dftRes);
-    cv::Mat mag;
-    cv::magnitude(dftRes[0], dftRes[1], mag);
+    // Expand the image to an optimal size.
+    cv::Mat padded;
+    int opt_rows = cv::getOptimalDFTSize(img.rows);
+    int opt_cols = cv::getOptimalDFTSize(img.cols);
+    cv::copyMakeBorder(img, padded, 0, opt_rows - img.rows , 0, opt_cols - img.cols, cv::BORDER_CONSTANT, cv::Scalar::all(0));
 
 
+    // Make place for both the real and complex values by merging planes into a
+    // cv::Mat with 2 channels.
+    // Use float element type because frequency domain ranges are large.
+    cv::Mat planes[] = {cv::Mat_<float>(padded), cv::Mat_<float>::zeros(padded.size())};
+    cv::Mat complex;
+    cv::merge(planes, 2, complex);
+
+    // Compute DFT
+    cv::dft(complex, complex);
+
+    // Split real and complex planes (you can reuse the 'planes' array to save the result)
+    cv::split(complex, planes);
+
+    // Compute the magnitude and phase (see e.g. cv::cartToPolar)
+    cv::Mat mag, phase;
+    cv::cartToPolar(planes[0], planes[1], mag, phase);
+
+    // Shift quadrants so the Fourier image origin is in the center of the image
+    dftshift(mag);
+  	
+  	// Switch to logarithmic scale to be able to display on screen
     mag += cv::Scalar::all(1);
     cv::log(mag, mag);
+
+    // Normalize for visualization
     cv::normalize(mag, mag, 0, 1, cv::NORM_MINMAX);
-    dftshift(mag);
+    //cv::normalize(phase, phase, 0, 1, cv::NORM_MINMAX);
 
-
-    //cv::imshow("im", I);
-    //cv::imshow("real", dftRes[0]);
-    //cv::imshow("imag", dftRes[1]);
-    //cv::imshow("mag", mag);
-    //cv::waitKey();
-    
-    cv::imwrite("freq_magnitude.bmp", mag);
+    // Save images 
+    cv::Mat magOut, phaseOut;
+    mag.convertTo(magOut, CV_8U, 255);
+    phase.convertTo(phaseOut, CV_8U, 255);
+    cv::imwrite("freq_magnitude.bmp", magOut);
+    cv::imwrite("freq_phase.bmp", phaseOut);
     
     return 0;
 }
